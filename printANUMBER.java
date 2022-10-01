d9cd72b3f0e28d1a60cbe23fb358ebd64848cd7a
Hemsss <113845400+Hemsss@users.noreply.github.com>
2022-10-01 06:04:08 -0700

    Add files via upload

---
+++ printANUMBER.java
@@ -0,0 +1,26 @@
+package printANUMBER;
+
+import java.util.Scanner;
+
+
+public class printANUMBER {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner ( System .in);
+		System.out.print(" enter a number:");
+		int number = reader . nextInt();
+		System.out.println("you entered:"+number);
+		
+		
+		
+		
+		
+	   
+	 
+	 
+		
+		
+
+	}
+
+}
