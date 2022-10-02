cf58c162f59768556345575a54a408a7d73fdefe
Hemsss <113845400+Hemsss@users.noreply.github.com>
2022-10-01 19:54:55 -0700

    Add files via upload

---
+++ FindAreaOfTriangle.java
@@ -0,0 +1,17 @@
+package areaoftriangle;
+import java.util.Scanner;
+public class FindAreaOfTriangle {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); 
+		 System.out.println("Enter the width of the Triangle:"); 
+		 double base = scanner.nextDouble(); 
+		 System.out.println("Enter the height of the Triangle:"); 
+		 double height = scanner.nextDouble(); 
+		 
+		 double area = (base* height)/2; 
+		 System.out.println("Area of Triangle is: " + area);
+
+	}
+
+}
---
+++ ReverseANumber.java
@@ -0,0 +1,24 @@
+package ReverseANumber;
+import java.util.Scanner;
+public class ReverseANumber {
+
+	public static void main(String[] args) {
+		int num=0; 
+		 int reversenum =0; 
+		 System.out.println("Input your number and press enter: "); 
+		 
+		 Scanner in = new Scanner(System.in); 
+		 
+		 num = in.nextInt(); 
+	
+		 while( num != 0 ) 
+		 { 
+		 reversenum = reversenum * 10; 
+		 reversenum = reversenum + num%10; 
+		 num = num/10; 
+		 } 
+		 System.out.println("Reverse of input number is:"+reversenum);
+
+	}
+
+}
