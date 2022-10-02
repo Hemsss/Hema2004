a6f88bd571ecf74a1dd67aab283035c66d37eae3
Hemsss <113845400+Hemsss@users.noreply.github.com>
2022-10-02 06:03:02 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int number;
+ printf( "Please enter a number: " );
+ scanf( "%d", &number);
+ printf( "You entered %d", number );
+ return 0;
+
+
+}
+
+
