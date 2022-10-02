9ffa696dbc3a2267a1dd2e49740935fb782a19ad
Hemsss <113845400+Hemsss@users.noreply.github.com>
2022-10-02 06:43:54 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 
 int main()
-{float num1;
- double num2;
- printf("Enter a number: ");
- scanf("%f", &num1);
- printf("Enter another number: ");
- scanf("%lf", &num2);
- printf("num1 = %f\n", num1);
- printf("num2 = %lf", num2);
+{int dividend, divisor, quotient, remainder;
+ printf("Enter dividend: ");
+ scanf("%d", &dividend);
+ printf("Enter divisor: ");
+ scanf("%d", &divisor);
+
+ quotient = dividend / divisor;
+
+ remainder = dividend % divisor;
+ printf("Quotient = %d\n", quotient);
+ printf("Remainder = %d", remainder);
  return 0;
 
 }
