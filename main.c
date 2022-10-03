60eeaa6de1d6f7b67c831979f1890b40ae19d5e0
Hemsss <113845400+Hemsss@users.noreply.github.com>
2022-10-03 01:23:29 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -4,21 +4,14 @@
 int main()
 {
 
-
-int n, i;
- unsigned long long fact = 1;
- printf("Enter an integer: ");
+int n, i, sum = 0;
+ printf("Enter a positive integer: ");
  scanf("%d", &n);
-
- if (n < 0)
- printf("Error! Factorial of a negative number doesn't exist.");
- else {
- for (i = 1; i <= n; ++i) {
- fact *= i;
+ i = 1;
+ while (i <= n) {
+ sum += i;
+ ++i;
  }
- printf("Factorial of %d = %llu", n, fact);
- }
+ printf("Sum = %d", sum);
  return 0;
-
-
 }
