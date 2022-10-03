81baef81b43ed40dfa14e6adfcf3b05051b890a2
Hemsss <113845400+Hemsss@users.noreply.github.com>
2022-10-03 07:38:53 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -3,17 +3,29 @@
 
 int main()
 {
-  int t1 = 0, t2 = 1, nextTerm = 0, n;
- printf("Enter a positive number: ");
- scanf("%d", &n);
 
- printf("Fibonacci Series: %d, %d, ", t1, t2);
- nextTerm = t1 + t2;
- while (nextTerm <= n) {
- printf("%d, ", nextTerm);
- t1 = t2;
- t2 = nextTerm;
- nextTerm = t1 + t2;
+char op;
+ double first, second;
+ printf("Enter an operator (+, -, *, /): ");
+ scanf("%c", &op);
+ printf("Enter two operands: ");
+ scanf("%lf %lf", &first, &second);
+ switch (op) {
+ case '+':
+ printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+ break;
+ case '-':
+ printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+ break;
+ case '*':
+ printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+ break;
+ case '/':
+ printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+ break;
+ // operator doesn't match any case constant
+ default:
+ printf("Error! operator is not correct");
  }
  return 0;
 }
